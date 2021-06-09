e3b159f9927f297f60086b45ccee8b9e32711f69
James Wong <jamesrwong310@gmail.com>
2021-06-08 19:34:35 -0700

    new changes

--- week 1/day 2/user_assignments.py
+++ week 1/day 2/user_assignments.py
@@ -8,36 +8,28 @@ class User:		# here's what we have so far
 
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
         self.account_balance += amount
-
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
 
     def display_user_balance(self):
         print(self.account_balance)
-
+        return self
 
 Harry = User('Harry', 'harrystyles@hotmail.com')
 Floyd = User('Floyd', 'thefloydmaywether@gmail.com')
 Donald = User('Donald', 'number45@gmail.com')
 
-Harry.make_deposit(100)
-Harry.make_deposit(250)
-Harry.make_deposit(200)
-Harry.make_withdrawal(150)
-Harry.display_user_balance()
+Harry.make_deposit(100).make_deposit(250).make_deposit(200).make_withdrawal(150).display_user_balance()
 
-Floyd.make_deposit(100000000)
-Floyd.make_deposit(420000000)
-Floyd.make_withdrawal(1000)
-Floyd.make_withdrawal(25000)
-Floyd.display_user_balance()
 
-Donald.make_deposit(200000000)
-Donald.make_withdrawal(200000)
-Donald.make_withdrawal(15000)
-Donald.make_withdrawal(10000)
-Donald.display_user_balance()
+Floyd.make_deposit(100000000).make_deposit(420000000).make_withdrawal(10000).make_withdrawal(25000).display_user_balance()
+
+
+Donald.make_deposit(200000000).make_withdrawal(200000).make_withdrawal(15000).make_withdrawal(10000).display_user_balance()
+
 # the specific user's account increases by the amount of the value received
 
